6031f4ae26f297e31de65e384c6c4b833a5cb778
sergioSHKLR <sergio@shklr.org>
2023-11-29 14:12:57 -0600

    pwa install

--- framework/pwa/sw.js
+++ framework/pwa/sw.js
@@ -1,3 +1,19 @@
+let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+    });
+
+    const installApp = document.getElementById('installApp');
+    installApp.addEventListener('click', async () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                deferredPrompt = null;
+            }
+        }
+    });
+
 // This is the "Offline page" service worker
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
